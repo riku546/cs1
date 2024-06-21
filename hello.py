3f73019b8b2edf6b84b7207a3114bc59f750e8e1
riku546 <91833521+riku546@users.noreply.github.com>
2024-06-21 11:37:47 +0900

    python

--- hello.py
+++ hello.py
@@ -1,6 +1,34 @@
-def hello(n):
-    for i in range(n):
-        print("hello world")
+# class Bank:
+#     def __init__(self , account) -> None:
+#         self.account = account
 
 
-hello(5)
+#     def withdraw(self , name , yen):
+#         money = self.account[name]
+#         if(money < yen):
+#             return None
+#         return money - yen
+    
+
+# bank = Bank({"bob":1234})
+# print(bank.withdraw("bob",234))
+# print(bank.withdraw("bob" , 9999))
+
+
+# class Rectangle:
+#     def __init__(self , w ,h) -> None:
+#         self.width = w
+#         self.height = h
+
+#     def area(self):
+#         return self.width * self.height
+    
+
+
+# r  = Rectangle(5,4)
+# print(r.width)
+# print(r.height)
+# print(r.area())
+
+
+
