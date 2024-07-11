61b233a63487057edd8f1d5817e88029533360ca
riku546 <91833521+riku546@users.noreply.github.com>
2024-07-11 17:45:11 +0900

    課題１　２

--- 13-2-1/js/assignment.js
+++ 13-2-1/js/assignment.js
@@ -1,18 +1,17 @@
 // Assignment
 
 //dom取得
-// const count = document.getElementById("count")
-const count = document.querySelector("p")
-console.log(count)
+const count = document.getElementById("count");
 
 let cnt = 0;
 count.textContent = 0;
 
 function plus() {
   cnt++;
-
+  count.textContent = cnt;
 }
 
 function minus() {
   cnt--;
+  count.textContent = cnt;
 }
--- 13-2-1/js/sample1.js
+++ 13-2-1/js/sample1.js
@@ -1,10 +1,10 @@
 // Sample 1
 alert("Hello!");
 
-let elements = document.getElementsByTagName("p");
-console.log(elements)
-let element = elements[0];
-console.log(element.textContent);
-// let elements = document.getElementsByClassName("sample-title")
-// let element = elements[1]
-// console.log(element.textContent)
+// let elements = document.getElementsByTagName("p");
+// console.log(elements)
+// let element = elements[0];
+// console.log(element.textContent);
+let elements = document.getElementsByClassName("sample-title")
+let element = elements[1]
+console.log(element.textContent)
