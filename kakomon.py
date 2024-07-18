1bce9ff0cd540d5d5602fdc3e267167925c970f9
riku <riku62560@gmail.com>
2024-07-18 18:53:12 +0900

    過去問

---
+++ kakomon.py
@@ -0,0 +1,35 @@
+
+
+# def ch05(x):
+#   if x % 5 == 0:
+#     return "OK"
+#   return "NG"
+
+# ans = ch05(80)
+# print(ans)
+# print(ch05(23))
+# print(ch05(35))
+# print(ch05(51))
+
+
+
+# def head2p(s):
+#   return s.replace(s[0] , "p")
+
+
+# ans = head2p('big')
+# print(ans)
+# print(head2p('apple'))
+# print(head2p('pen'))
+# print(head2p('cut'))
+# print(head2p('orange'))
+
+
+# def gt10(xs):
+#    return sum([i for i in xs if i > 10])
+
+# ans = gt10([3, 19, 11])
+# print(ans)
+# print(gt10([15, 7, 6, 21]))
+# print(gt10([7, 11, 32]))
+# print(gt10([16, 24, 22]))
