7d18787f80418c177f391628e976ae4509429c0a
riku546 <91833521+riku546@users.noreply.github.com>
2024-07-19 15:18:46 +0900

    課題１

--- locker-sample/js/room_status.js
+++ locker-sample/js/room_status.js
@@ -1,13 +1,28 @@
 let BASE_URL = "https://edu-iot.iniad.org/api/v1";
 
 function displayRoomStatus(result) {
+  console.log(result);
+  //結果を表示するdom
+  const resultStatus = document.getElementById("monitoring-result")
+  const roomTemp = document.getElementById("room-temperature");
+  const roomHum = document.getElementById("room-humidity");
+  const roomAirpres = document.getElementById("room-airpressure");
+  const roomIll = document.getElementById("room-illuminance");
 
+  //domにresultの値をセット
+  resultStatus.textContent = result["status"];
+  roomTemp.textContent = result["temperature"];
+  roomHum.textContent = result["humidity"];
+  roomAirpres.textContent = result["airpressure"];
+  roomIll.textContent = result["illuminance"];
 }
 
 function getRoomStatus() {
-  // let userid =
-  // let userpw =
-  // let roomNum =
-  // let url =
-  callRoomStatusAPI(url, 'GET', userid, userpw, displayRoomStatus);
+  //input要素のvalue取得
+  let userid = document.getElementById("iniad-id").value;
+  let userpw = document.getElementById("iniad-pw").value;
+  let roomNum = document.getElementById("room-number").value;
+
+  let url = `https://edu-iot.iniad.org/api/v1/sensors/${roomNum}`;
+  callRoomStatusAPI(url, "GET", userid, userpw, displayRoomStatus);
 }
