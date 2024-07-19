8e8023507436fa29ad72c5297ad68eda5dcfdb3a
riku546 <91833521+riku546@users.noreply.github.com>
2024-07-19 14:56:52 +0900

    ロッカーフォルダーをダウンロード

---
+++ locker-sample/js/locker_info.js
@@ -0,0 +1,18 @@
+
+let BASE_URL = "https://edu-iot.iniad.org/api/v1";
+
+function displayLockerPosition(result) {
+
+}
+
+function displayIccardInformation(result) {
+
+}
+
+function getLockerPosition() {
+
+}
+
+function getRegisteredIccard() {
+
+}
